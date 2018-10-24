37214d41d8842edac4862b8b6ab53b1ce74a287a
RapidCaptain <rapidcaptainli@gmail.com>
2018-10-24 22:21:54 +1100

    Add methods

--- src/main/java/com/example/demo/controller/CommodityController.java
+++ src/main/java/com/example/demo/controller/CommodityController.java
@@ -26,4 +26,38 @@ public class CommodityController {
 
     @Autowired
     BuyRepository buyRepository;
+	
+	@PostMapping(value="/upload")
+	public RestEntity release(MultipartFile file) {
+	}
+	
+    @GetMapping(value="/obtain/{username}/{pageSize}/{pageIndex}")
+    public RestEntity obtain(@PathVariable String username, @PathVariable Integer pageSize, @PathVariable Integer pageIndex) {
+	}
+	
+    @PostMapping(value="/add")
+    public RestEntity add(@RequestBody CommodityDTO commodityDTO) {
+	}
+	
+    @GetMapping("/listOrder/{username}/{pageSize}/{pageIndex}")
+    public RestEntity listBuyDTO(@PathVariable String username,@PathVariable Integer pageSize,@PathVariable Integer pageIndex){
+	}
+	
+	@GetMapping("/listOrder/{pageSize}/{pageIndex}")
+    public RestEntity listAllBuyDTO(@PathVariable Integer pageSize,@PathVariable Integer pageIndex){
+	}
+	
+	@PostMapping("/okOrder/{id}/{commodity}/{count}")
+    @Transactional
+    public RestEntity okOrder(@PathVariable Integer id,@PathVariable String commodity,@PathVariable Integer count){
+	}
+	
+	@PostMapping("/cancleOrder/{id}")
+    public RestEntity cancleOrder(@PathVariable Integer id){
+	}
+	
+	@GetMapping("/getDetail/{id}")
+    public RestEntity getDetail(@PathVariable String id){
+	}
+	 
 }
