0fe5be632c2a38493ed662a6ad75fdfb0ea463b8
RapidCaptain <rapidcaptainli@gmail.com>
2018-10-24 23:49:53 +1100

    initial RestEntity class

--- src/main/java/com/example/demo/model/RestEntity.java
+++ src/main/java/com/example/demo/model/RestEntity.java
@@ -3,18 +3,17 @@ package com.example.demo.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.example.demo.enums.RestStatusEnum;
 import lombok.Data;
 import lombok.ToString;
 
-import javax.validation.constraints.Null;
 import java.io.Serializable;
 
 /**
- * @author qinzhongjian
- * @date created in 2018/6/25 22:41
+ * @author haocun li
+ * @date created in 2018/10/24 23:42
  * @since 1.0.0
  */
+
 @Data
 @ToString
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -24,13 +23,13 @@ public class RestEntity implements Serializable {
     private static final long serialVersionUID = 3550224421750657701L;
 
     /**
-     * [M] 平台状态码
+     * [M] condition code
      */
     @JsonProperty("code")
     private Integer code;
 
     /**
-     * [M] 错误信息
+     * [M] error message
      */
     @JsonProperty("msg")
     private String msg;
@@ -53,32 +52,4 @@ public class RestEntity implements Serializable {
     @JsonProperty("details")
     private Object details;
 
-    public RestEntity(RestStatusEnum restStatusEnum, @Null String username,@Null String role) {
-        this.code = restStatusEnum.getCode();
-        this.msg = restStatusEnum.getMessage();
-        if (username != null) {
-            this.username = username;
-        }
-        if (role != null) {
-            this.role = role;
-        }
-    }
-
-    public RestEntity(RestStatusEnum restStatusEnum, @Null Object details) {
-        this.code = restStatusEnum.getCode();
-        this.msg = restStatusEnum.getMessage();
-        if (details != null) {
-            this.details = details;
-        }
-
-    }
-    public static RestEntity ok(RestStatusEnum restStatusEnum, @Null String username,@Null String role){
-        return new RestEntity(restStatusEnum,username,role);
-    }
-    public static RestEntity ok(Object details){
-        return new RestEntity(RestStatusEnum.OK,details);
-    }
-    public static RestEntity error(RestStatusEnum restStatusEnum, @Null Object error){
-        return new RestEntity(restStatusEnum,error);
-    }
 }
