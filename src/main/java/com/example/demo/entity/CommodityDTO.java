6d48ad4a36d8e34466fb7b4d727402b81c8d4927
RapidCaptain <rapidcaptainli@gmail.com>
2018-10-28 17:12:06 +1100

    DTO class of commodity

--- src/main/java/com/example/demo/entity/CommodityDTO.java
+++ src/main/java/com/example/demo/entity/CommodityDTO.java
@@ -5,8 +5,8 @@ import lombok.Data;
 import javax.persistence.*;
 
 /**
- * @author qinzhongjian
- * @date created in 2018/10/17 08:45
+ * @author haocunli
+ * @date created in 2018/10/20 17:45
  * @since 1.0.0
  */
 @Data
@@ -14,14 +14,14 @@ import javax.persistence.*;
 @Entity
 public class CommodityDTO {
     @Id
-    private String id;//商品编号
-    private String name;//商品名称
-    private String username;//商家
-    private String picture;//图片
-    private String category;//类别
-    private String introduction;//简介
-    private Double price;//价格
-    private Integer lave;//剩余件数
-    private Integer sell;//售出件数
+    private String id;
+    private String name;
+    private String username;
+    private String picture;
+    private String category;
+    private String introduction;
+    private Double price;
+    private Integer lave;
+    private Integer sell;
 
 }
