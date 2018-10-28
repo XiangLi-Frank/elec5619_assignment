349972aa4c7afe4fe9982a2c721da3d8eb705b55
RapidCaptain <rapidcaptainli@gmail.com>
2018-10-28 17:01:59 +1100

    initial Mallcontroller class and add get list

--- src/main/java/com/example/demo/controller/MallController.java
+++ src/main/java/com/example/demo/controller/MallController.java
@@ -1,25 +1,23 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.BuyDTO;
 import com.example.demo.entity.CommodityDTO;
 import com.example.demo.model.RestEntity;
 import com.example.demo.repository.BuyRepository;
 import com.example.demo.repository.CommodityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * @author qinzhongjian
- * @date created in 2018/10/19 10:42
+ * @author haocunli
+ * @date created in 2018/10/24 10:42
  * @since 1.0.0
  */
-@RestController
-public class MallController {
+
+public class MallController1 {
 
     @Autowired
     CommodityRepository commodityRepository;
@@ -30,40 +28,9 @@ public class MallController {
     @GetMapping("/listCommodityDTO/{category}")
     public RestEntity listCommodityDTO(@PathVariable String category){
         List<CommodityDTO> commodityDTOS = this.commodityRepository.findAll();
-        //获取库存不为0的商品
+        //get commodity that its stock is not empty
         commodityDTOS = commodityDTOS.stream().filter(commodityDTO -> commodityDTO.getLave() != 0)
                 .filter(commodityDTO -> commodityDTO.getCategory().equals(category)).collect(Collectors.toList());
-        return RestEntity.ok(commodityDTOS);
-    }
-
-    @PostMapping("/buyCommodityDTO")
-    public RestEntity buyCommodityDTO(@RequestBody BuyDTO buyDTO){
-        this.buyRepository.save(buyDTO);
-        return RestEntity.ok(buyDTO);
-    }
-
-    /**
-     * description: <br>
-     * 买家订单信息
-     * @param: [buyDTO]
-     * @return: com.example.demo.model.RestEntity
-     * @method：buyCommodityDTO
-     */
-
-    @GetMapping("/listBuyDTO/{username}/{pageSize}/{pageIndex}")
-    public RestEntity listBuyDTO(@PathVariable String username, @PathVariable Integer pageSize, @PathVariable Integer pageIndex){
-        PageRequest pageRequest = new PageRequest(pageIndex-1,pageSize);
-        Page<BuyDTO> buyDTOS =  this.buyRepository.findAllByUsername(username,pageRequest);
-        return RestEntity.ok(buyDTOS);
-    }
-
-    @GetMapping("/listCommodityDTO/{category}/{keyword}")
-    public RestEntity listCommodityDTO(@PathVariable String category,@PathVariable String keyword){
-        List<CommodityDTO> commodityDTOS = this.commodityRepository.findAll();
-        //获取库存不为0的商品
-        commodityDTOS = commodityDTOS.stream().filter(commodityDTO -> commodityDTO.getLave() != 0)
-                .filter(commodityDTO -> commodityDTO.getCategory().equals(category))
-                .filter(commodityDTO -> commodityDTO.getName().indexOf(keyword) != -1).collect(Collectors.toList());
         return RestEntity.ok(commodityDTOS);
     }
 }
